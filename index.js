a55be774aaaa78460bc2b61c0dd9e110324b8d2a
roopa26 <roopashree65@gmail.com>
2020-09-19 12:50:16 +0400

    index.js includes key press event

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-var imgSrc1 = "images/dice"+randomNumber1+".png";
-var imgSrc2 = "images/dice"+randomNumber2+".png";
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-document.querySelector(".img1").setAttribute("src",imgSrc1);
-document.querySelector(".img2").setAttribute("src",imgSrc2);
-
-if(randomNumber1>randomNumber2){
-  document.querySelector(".container h1").textContent = "Player 1 wins!";
+for(var i =0;i<numberOfDrumButtons;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",btnClickEventListener)
 }
-else if(randomNumber1<randomNumber2){
-  document.querySelector(".container h1").textContent = "Player 2 wins!";
+function btnClickEventListener(event){
+  var classAttribute = this.getAttribute('class')[0];
+  var soundTrack = "sounds/";
+  switch(classAttribute){
+    case "w": soundTrack+="crash.mp3";break;
+    case "a": soundTrack+="kick-bass.mp3";break;
+    case "s": soundTrack+="snare.mp3";break;
+    case "d": soundTrack+="tom-1.mp3";break;
+    case "j": soundTrack+="tom-2.mp3";break;
+    case "k": soundTrack+="tom-3.mp3";break;
+    case "l": soundTrack+="tom-4.mp3";break;
+    default : console.log("invalid key"); break;
+  }
+  var audio = new Audio(soundTrack);
+  audio.play();
 }
-else
-{
-  document.querySelector(".container h1").textContent = "draw";
-}
+document.addEventListener("keypress",function(event){
+  var classAttribute = event.key;
+  var soundTrack = "sounds/";
+  switch(classAttribute){
+    case "w": var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+    case "a": var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+    case "s": var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+    case "d": var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+    case "j": var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+    case "k": var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+    case "l": var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+    default : console.log("invalid key");
+    break;
+  }
+})
+//var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();
